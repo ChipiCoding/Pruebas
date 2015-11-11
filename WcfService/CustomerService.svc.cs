7eed9fe4ec58480d3dcb5bf7930d0f35342572e6
Andres Mauricio Zapata Caro <andres.zpt@outlook.com>
2015-11-11 15:16:40 -0500

    ok

---
+++ WcfService/CustomerService.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfService.ClassSerializer;
+
+namespace WcfService
+{
+    public class CustomerService : ICustomerService
+    {
+        private PruebaDBEntities DBContext = new PruebaDBEntities();
+
+        public List<CustomerSerializer> GetAll()
+        {          
+
+            var query = (from C in DBContext.Customers
+                         select C).Distinct();
+
+            List<CustomerSerializer> Lista = new List<CustomerSerializer>();
+
+            query.ToList().ForEach(rec =>
+            {
+                Lista.Add(new CustomerSerializer
+                {
+                    id = rec.id,
+                    idDocumentType = rec.idDocumentType,
+                    DocumentsType = new DocumentsTypeSerializer { id = rec.DocumentsType.id, description =rec.DocumentsType.description},
+                    documentNumber = rec.documentNumber,
+                    birthdate = rec.birthdate,
+                    idCity = rec.idCity,
+                    City = new CitySerializer { id = rec.City.id, name = rec.City.name },
+                    movilNumber = rec.movilNumber,
+                    name = rec.name
+                });
+            });
+
+            return Lista;
+        }
+    }
+}
